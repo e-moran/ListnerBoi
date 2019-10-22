c9745fee258f77ee92ad95cdcee2194db001bf30
Eoin Moran <eoin.t.moran00@gmail.com>
2019-10-22 14:37:21 +0100

    Modified the Listner class to: -Give timestamps for start of recording and end. -Correctly schedule recordings leaving a 1 minute buffer on either side -Output the output from ffmpeg to stdout to avoid an issue where ffmpeg stops because it's stdout buffer fills up

--- src/ie/eointm/listnerboi/Listner.java
+++ src/ie/eointm/listnerboi/Listner.java
@@ -1,8 +1,8 @@
 package ie.eointm.listnerboi;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -21,17 +21,18 @@ public class Listner {
     public void addTimers() {
         for(int i=0; i<config.getSchedule().size(); i++) {
             Show s = config.getSchedule().get(i);
-            ListnerThread t = new ListnerThread(config.getStreamURL(), s.getRuntime(), config.getOutputDir(), s.getShowName(), this);
+            ListnerThread t = new ListnerThread(this, s, config);
             Calendar d = Calendar.getInstance();
 
             d.set(Calendar.DAY_OF_WEEK, s.getDay());
-            d.set(Calendar.HOUR, s.getStartHr());
+            d.set(Calendar.HOUR_OF_DAY, s.getStartHr());
             d.set(Calendar.MINUTE, s.getStartMin());
             d.set(Calendar.SECOND, s.getStartSec());
             d.set(Calendar.MILLISECOND, 0);
+            d.add(Calendar.MINUTE, -1);
 
-            timer.schedule(new TimedListner(t), d.getTime(), 1000L * 60L * 60L * 24L * 7L);
-            System.out.println("Scheduled timer for "+s.getShowName());
+            timer.schedule(new TimedListner(t), d.getTime(), 1000 * 60 * 60 * 24 * 7);
+            System.out.println("Scheduled timer for "+s.getShowName() + "\nFirst time at " + d.getTimeInMillis());
         }
     }
 
@@ -49,55 +50,58 @@ public class Listner {
 
     class ListnerThread extends Thread {
         private Listner listner;
-        private int timeToListen;
-        private String streamUrl;
         private String outputLocation;
+        private String filename;
+        private Show s;
+        private Config c;
 
-        public ListnerThread(String streamUrl, int timeToListen, String outputDir, String showName, Listner listner) {
-
+        public ListnerThread(Listner listner, Show s, Config c) {
             this.listner = listner;
-            this.timeToListen = timeToListen;
-            //this.outputLocation = outputDir + "/" + showName + "_" + System.currentTimeMillis() + ".mp3";
-            this.outputLocation = showName + "_" + System.currentTimeMillis() + ".mp3";
-            this.streamUrl = streamUrl;
+            this.s = s;
+            this.c = c;
+
         }
 
         public void run() {
+            this.filename = s.getShowName() + "_" + System.currentTimeMillis() + ".mp3";
+            this.outputLocation = c.getOutputDir() + "/" + filename;
+
+            System.out.println(getTimestamp() + s.getShowName() + " is being saved to " + outputLocation);
             streamToFile();
+
         }
 
         private void streamToFile() {
-            String[] command = {"ffmpeg", "-t", Integer.toString(timeToListen), "-i", streamUrl, "-acodec", "mp3", "-ab", "257k", outputLocation};
+            String[] command = {"ffmpeg", "-t", Integer.toString(s.getRuntime()+120), "-i", c.getStreamURL(), "-acodec", "mp3", "-ab", "257k", outputLocation};
+            Process p = null;
             synchronized (listner) {
                 try {
-                    String s = "";
-                    Process p = new ProcessBuilder(command).start();
-
-                    BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                    BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-                    StringBuffer start= new StringBuffer();
-                    // read the output from the command
-                    while ((s = stdInput.readLine()) != null)
-                    {
-                        System.out.println(s);
-                    }
-                    stdInput.close();
-                    // read any errors from the attempted command
-                    while ((s = stdError.readLine()) != null)
-                    {
-                        System.out.println(s);
-                    }
+                    p = new ProcessBuilder(command).inheritIO().start();
+                    p.waitFor();
 
                     postStream(true);
                 } catch (IOException e) {
                     e.printStackTrace();
                     postStream(false);
+                } catch (InterruptedException e) {
+                    if(p != null)
+                        p.destroy();
+                    e.printStackTrace();
                 }
             }
         }
 
         private void postStream(boolean succeeded) {
-            System.out.println("Stream stored at " + outputLocation);
+            System.out.println(getTimestamp() + "Stream successfully recorded to location " + outputLocation);
+
+            new PostRecordingEmail(c, s, filename);
+        }
+
+        private String getTimestamp() {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+
+            return '[' + dtf.format(now) + "] ";
         }
     }
 }
